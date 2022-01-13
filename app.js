89be791127adf52ddef89016defb20cbf92dcbcd
Payal <singhpayal2503@gmail.com>
2022-01-12 17:04:35 -0800

    Express Sessions Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +16,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 const { signedCookie, signedCookies } = require('cookie-parser');
+const req = require('express/lib/request');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -33,11 +36,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(CookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth (req, res, next) {
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -51,7 +63,8 @@ function auth (req, res, next) {
     var password = auth[1];
 
     if (user == 'admin' && password == 'password') {
-        res.cookie('user','admin',{signed: true});
+      req.session.user = 'admin';
+        // res.cookie('user','admin',{signed: true});
         next(); // authorized
     } 
     else {
@@ -62,7 +75,7 @@ function auth (req, res, next) {
     }
   }
   else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           next();
       }
       else {
