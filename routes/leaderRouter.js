efd284c1741950900213f7e81b925228e717342a
Payal <singhpayal2503@gmail.com>
2022-01-14 16:06:23 -0800

    User Authentication with Passport and JSON Web Token(JWT)

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var authenticate = require('../authenticate');
 const Leaders = require('../models/leaders')
 const leaderRouter = express.Router();
 
@@ -19,7 +20,7 @@ leaderRouter.route('/')
     .catch((err) => next(err));
 })
 
-.post((req , res , next) => {
+.post(authenticate.verifyUser, (req , res , next) => {
     Leaders.create(req.body)
     .then((leaders) =>{
         console.log('Leader Created', leaders);
@@ -30,12 +31,12 @@ leaderRouter.route('/')
     .catch((err) => next(err))
 })
 
-.put((req , res , next) => {
+.put(authenticate.verifyUser, (req , res , next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leaders');
 })
 
-.delete((req , res , next) => {
+.delete(authenticate.verifyUser, (req , res , next) => {
     Leaders.remove({})
     .then((resp) =>{
         res.statusCode= 200,
@@ -57,12 +58,12 @@ leaderRouter.route('/:leaderId')
     .catch((err) => next(err));
 })
 
-.post((req , res , next) => {
+.post(authenticate.verifyUser, (req , res , next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/' +req.params.leaderId);
 })
 
-.put((req , res , next) => {
+.put(authenticate.verifyUser, (req , res , next) => {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, { new: true })
@@ -78,7 +79,7 @@ leaderRouter.route('/:leaderId')
 })
 
 
-.delete((req , res , next) => {
+.delete(authenticate.verifyUser, (req , res , next) => {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((resp) =>{
         res.statusCode= 200,
